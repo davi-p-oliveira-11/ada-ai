9ee7c47ca32e352830702801414d0124c06fd94a
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-11-17 19:48:14 -0300

    added changes to small card

--- app/components/SmallStatCard.tsx
+++ app/components/SmallStatCard.tsx
@@ -1,15 +1,13 @@
 export default function SmallStatCard() {
   return (
-    <div className="bg-[#121212] w-full md:w-[390px] h-[434px] rounded-xl p-8 flex flex-col justify-between">
-      
-      <h2 className="text-white text-[32px] font-semibold leading-tight">
-        Your Small Card Title
+    <div className="bg-[#121212] w-full md:w-[390px] h-[415px] rounded-xl p-8 flex flex-col justify-center">
+      <h2 className="text-white text-[32px] font-normal leading-tight text-left mb-10">
+        AI-powered automation built for CX teams
       </h2>
 
-      <button className="px-6 py-3 bg-gradient-to-r from-[#9977D4] to-[#6337AE] text-white font-semibold rounded-lg mt-6">
-        Call To Action
+      <button className="px-6 py-3 bg-linear-to-r from-[#9977D4] to-[#6337AE] text-white font-semibold rounded-lg">
+        View all Case Studies
       </button>
-
     </div>
   );
 }
